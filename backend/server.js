ee5dc0e36c855df89441a5e65dadbfc24a21aefa
Artak <safaryanartak81@gmail.com>
2026-06-25 00:20:35 +0400

    Fix nodemailer connection by adding family 4 and logging

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,10 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    socketTimeout: 10000,
+    logger: true,
+    debug: true
 });
 const connectDB = async () => {
     try {
