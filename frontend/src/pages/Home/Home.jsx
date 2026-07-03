dd34cf4262f8f65d429306cffcb554063473322a
Artak <safaryanartak81@gmail.com>
2026-07-04 01:08:00 +0400

    Change

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,3 +1,38 @@
+// import React from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+// import styles from './Home.module.css';
+// import SlideOne from './SlideOne';
+// import SlideTwo from './SlideTwo';
+// import SlideThree from './SlideThree';
+// import RecommendedBooks from '../RecommendedBooks/RecommendedBooks';
+
+// const Home = () => {
+//   return (
+//     <>
+//       <RecommendedBooks />
+//       <Swiper
+//         autoplay={{ delay: 5000, disableOnInteraction: false }}
+//         pagination={{ clickable: true }}
+//         navigation={true}
+//         modules={[Autoplay, Pagination, Navigation]}
+//       >
+//         <SwiperSlide><SlideOne /></SwiperSlide>
+//         <SwiperSlide><SlideTwo/></SwiperSlide>
+//         <SwiperSlide><SlideThree/></SwiperSlide>
+//       </Swiper>
+
+
+//   </>
+//   );
+// };
+
+// export default Home;
+
+
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
@@ -18,15 +53,16 @@ const Home = () => {
         autoplay={{ delay: 5000, disableOnInteraction: false }}
         pagination={{ clickable: true }}
         navigation={true}
+        autoHeight={true}
         modules={[Autoplay, Pagination, Navigation]}
       >
         <SwiperSlide><SlideOne /></SwiperSlide>
-        <SwiperSlide><SlideTwo/></SwiperSlide>
-        <SwiperSlide><SlideThree/></SwiperSlide>
+        <SwiperSlide><SlideTwo /></SwiperSlide>
+        <SwiperSlide><SlideThree /></SwiperSlide>
       </Swiper>
 
-    
-  </>
+
+    </>
   );
 };
 
