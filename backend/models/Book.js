5b22daa4b0f707bbba8b375fd6ba39190c57a7f6
Artak <safaryanartak81@gmail.com>
2026-07-07 21:14:33 +0400

    Add g

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -4,6 +4,7 @@ const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   price: { type: Number, required: true },
+  genre: { type: String, required: true, default: "other" },
   image: { type: String, default: "https://via.placeholder.com/150" }
 });
 
