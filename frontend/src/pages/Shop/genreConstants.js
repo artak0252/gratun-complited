c7f40376cc0204e9eaf034b5f12590c2b6cdf580
Artak <safaryanartak81@gmail.com>
2026-07-09 14:04:49 +0400

    W

--- frontend/src/pages/Shop/genreConstants.js
+++ frontend/src/pages/Shop/genreConstants.js
@@ -4,11 +4,11 @@ export const bookGenres = [
           { id: 'history', label: 'Պատմական' },
           { id: 'philosophy', label: 'Փիլիսոփայական' },
           { id: 'poetry', label: 'Պոեզիա' },
-          { id: 'fantasy', label: 'Ֆենթեզի' },
+          { id: 'fantasy', label: 'Ֆամտաստիկ' },
           { id: 'detective', label: 'Դետեկտիվ' },
           { id: 'children', label: 'Մանկական' },
           { id: 'biography', label: 'Կենսագրական' },
-          { id: 'science', label: 'Գիտական' },
+          { id: 'science', label: 'Մոտիվացիոն' },
           { id: 'spiritual', label: 'Հոգևոր-կրոնական' },
           { id: 'other', label: 'Այլ' }
 ];
