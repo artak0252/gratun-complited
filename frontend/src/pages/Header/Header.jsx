efcef2256e2c8bd4a0a1bbe6e559c635775c5f4c
Artak <safaryanartak81@gmail.com>
2026-07-09 15:34:40 +0400

    W

--- frontend/src/pages/Header/Header.jsx
+++ frontend/src/pages/Header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
     <header className={styles.headerContainer}>
       <div className={styles.headerTop}>
         <div className={styles.headerLogo}>
-          <Link to="/">ԳրատունՀաբ</Link>
+          <Link to="/">Գրատուն-Հաբ</Link>
         </div>
 
         <div className={styles.headerRight}>
